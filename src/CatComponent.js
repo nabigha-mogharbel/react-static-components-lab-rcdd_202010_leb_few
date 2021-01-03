d98db40b7968ae6109ada174cbe2ee44e4b0f53b
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2021-01-03 18:52:37 +0000

    Done.

--- src/CatComponent.js
+++ src/CatComponent.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 export default class CatComponent extends Component {
   render() {
     return (
-      <div className="bar" id="cat">
-        <img src="/cat.gif" />
+      <div className="bar" id="cat" >
+        <img alt="cat" src="/cat.gif" />
       </div>
     );
   }
